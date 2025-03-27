ab3acc32150b2f659cba572c443bcf5658a49241
Ayse Atik <moonloversin@gmail.com>
2025-03-27 23:17:04 +0300

    general structure is created

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.sidebar-toggle').addEventListener('click', function() {
+        document.querySelector('#sidebar').classList.toggle('active');
+    });
+});
